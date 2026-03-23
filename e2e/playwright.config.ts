3e43fa7ea596c4bbfa8d5c65641eebaa7654ab28
JT Archie <jtarchie@gmail.com>
2026-03-22 21:12:52 -0600

    e2e: enable race detector on webserver

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   // Start the CI server before running tests
   webServer: {
     command:
-      "go run ../main.go server --storage-sqlite-path=e2e-test.db --port 8080 --secrets-sqlite-path=e2e-secrets.db --secrets-sqlite-passphrase=testing",
+      "go run -race ../main.go server --storage-sqlite-path=e2e-test.db --port 8080 --secrets-sqlite-path=e2e-secrets.db --secrets-sqlite-passphrase=testing",
     url: "http://localhost:8080/health",
     reuseExistingServer: !process.env.CI,
     timeout: 30000,
